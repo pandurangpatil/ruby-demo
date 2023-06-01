b7a0f739315cf9d00df4156544f62957c1032f87
Karan <karan.batavia@privado.ai>
2023-06-01 16:49:50 +0530

    fix flow

--- sink.rb
+++ sink.rb
@@ -1,15 +1,8 @@
-require './source'
-
 def print_customer(customer_id)
     puts customer_id
 end
 
-def process_customer
-    cus_id = get_customer_id()
-    print_customer(cus_id)
-end
 
-process_customer()
 
 
 

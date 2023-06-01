9ccb3dbc74cb8540f4f122866674d5482e9d1b40
Karan <karan.batavia@privado.ai>
2023-06-01 15:43:44 +0530

    first commit

---
+++ customer.rb
@@ -0,0 +1,5 @@
+module Customer
+    $customer_id='my_customer_id'
+    CUSTOMER_CREDIT_CARD='customer_credit_card_number'
+    $date_of_birth='my_dob'
+end
---
+++ person_identification.rb
@@ -0,0 +1,4 @@
+module Person
+    $passport_number='passport_num'
+    DL_NUMBER='dl_number'
+end

9c4fe93eb50c81179286404c3818ae1f98125481
Karan <karan.batavia@privado.ai>
2023-06-02 15:56:51 +0530

    added a file to show flows

---
+++ sample.rb
@@ -0,0 +1,30 @@
+passport_number = "abc"
+y = 1
+a = passport_number
+
+
+
+if y < 2
+    b = a + 1
+else
+    b = a + 2
+end
+
+
+z = b
+
+x = 4
+while x > 0
+    c += z + 2
+    x -= 1
+end
+
+
+for j in 1..5
+    d -= c + 2
+end
+
+
+e = d
+
+puts e
